748d25044361b824a30211e67b17b76d69003180
SethMcn <107069935+SethMcN@users.noreply.github.com>
2025-01-04 17:57:53 +0000

    new slide system, hide and show layers

--- electron-app/src/renderer/src/App.jsx
+++ electron-app/src/renderer/src/App.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from 'react'
-import ToolBar from "./components/ToolBar"
-import SlideBar from "./components/SlideBar"
-import MainCanvas from "./components/MainCanvas"
+import ToolBar from './components/ToolBar'
+import SlideBar from './components/SlideBar'
+import MainCanvas from './components/MainCanvas'
 
 function App() {
-
   const [canvas, setCanvas] = useState(null)
-
-
+  const [canvases, setCanvases] = useState([])
 
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
   return (
     <>
-    <div className="The-One-Container">
-      <div className="ToolBar">
-        <ToolBar canvas={canvas} />
+      <div className="The-One-Container">
+        <div className="ToolBar">
+          <ToolBar canvas={canvas} />
+        </div>
+        <div className="Canvas-SlideBar">
+          <SlideBar />
+          <MainCanvas
+            canvas={canvas}
+            setCanvas={setCanvas}
+            canvases={canvases}
+            setCanvases={setCanvases}
+          />
+        </div>
       </div>
-      <div className="Canvas-SlideBar">
-        <SlideBar />
-        <MainCanvas canavs={canvas} setCanvas={setCanvas} />
-      </div>
-    </div>
     </>
   )
 }
 export default App
-
-
--- electron-app/src/renderer/src/components/MainCanvas.jsx
+++ electron-app/src/renderer/src/components/MainCanvas.jsx
@@ -2,15 +2,54 @@ import React, { useRef, useState, useEffect } from 'react'
 import { Canvas } from 'fabric'
 import { handleObjectMoving, clearGuidelines } from './snappingHelpers'
 
-export default function MainCanvas({ setCanvas }) {
+export default function MainCanvas({ canvas, setCanvas, canvases, setCanvases }) {
   const canvasRef = useRef(null)
   const [guidelines, setGuidelines] = useState([])
+
+  const addSlide = () => {
+    console.log(canvas)
+    // Retrieve all objects on the canvas
+    const allObjects = canvas.getObjects()
+
+    // Iterate over the objects and perform actions
+    allObjects.forEach((obj, index) => {
+      console.log(`Object ${index}:`, obj)
+      obj.set({ visible: false })
+    })
+
+    const layer = canvas.getObjects()
+    setCanvases((canvases) => [...canvases, layer])
+
+    canvas.renderAll()
+  }
+
+  const nextSlide = () => {
+    // first hide all objects on current layer
+
+    // Retrieve all objects on the canvas
+    const allObjects = canvas.getObjects()
+
+    // Iterate over the objects and perform actions
+    allObjects.forEach((obj, index) => {
+      console.log(`Object ${index}:`, obj)
+      obj.set({ visible: false })
+    })
+
+    // then show all objects on next layer
+    const layer = canvases.indexof(canvas).getObjects()
+    layer.forEach((obj, index) => {
+      console.log(`Object ${index}:`, obj)
+      obj.set({ visible: true })
+    })
+
+    canvas.renderAll()
+  }
 
   useEffect(() => {
     if (canvasRef.current) {
       let width = window.innerWidth
       let height = window.innerHeight
-      const initCanvas = new Canvas(canvasRef.current, {
+      let initCanvas = new Canvas(canvasRef.current, {
         width: width * 0.75,
         height: height * 0.75
       })
@@ -19,6 +58,7 @@ export default function MainCanvas({ setCanvas }) {
       initCanvas.renderAll()
 
       setCanvas(initCanvas)
+      setCanvases((canvases) => [...canvases, initCanvas])
 
       initCanvas.on('object:moving', (event) => {
         handleObjectMoving(initCanvas, event.target, guidelines, setGuidelines)
@@ -38,6 +78,8 @@ export default function MainCanvas({ setCanvas }) {
     <div className="Container-Canvas">
       <div className="Container-Canvas">
         <canvas id="canvas" ref={canvasRef}></canvas>
+        <button onClick={addSlide}>Add Slide</button>
+        <button onClick={nextSlide}>Next Slide</button>
       </div>
     </div>
   )
